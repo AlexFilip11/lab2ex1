4bb84b1b487d38140776aa1745f89f2f67048967
Alex Filip <alexfilippaul@gmail.com>
2022-12-04 22:59:25 +0200

    Add project files.

---
+++ lab2ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab2ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrieti un program care va afisa suma cifrelor unui numar n, n fiind citit de la tastatura.
+            Console.WriteLine("Introduceti un numar: ");
+            int n = int.Parse(Console.ReadLine());
+            int suma=0;
+            while(n!=0)
+            {
+                suma = suma + n % 2;
+                n = n / 10;
+            }
+            Console.WriteLine("Suma cifrelor este: "+suma);
+        }
+    }
+}
